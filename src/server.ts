841ebaa8209b4746cab69bf1ec16b02009596241
Eric Wong <wymeric89@gmail.com>
2018-01-23 15:29:23 +0800

    allow authorization header

--- src/server.ts
+++ src/server.ts
@@ -87,7 +87,7 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   // After successful login, redirect back to the intended page
   if (!req.user &&
       req.path !== "/login" &&
